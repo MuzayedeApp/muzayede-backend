9b0eecbbd239ebd7b96223399337b3b6d573913b
yagizpancak <yagizcan923@gmail.com>
2024-03-16 18:01:28 +0300

    initial commit

---
+++ src/main/java/com/cengo/muzayedebackend/MuzayedeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cengo.muzayedebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzayedeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzayedeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cengo/muzayedebackend/MuzayedeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cengo.muzayedebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzayedeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
