968e0176a34142afc48dd7e99015afc108e1ebea
yagizpancak <yagizcan923@gmail.com>
2024-03-19 20:48:10 +0300

    postgre actions

--- src/test/java/com/cengo/muzayedebackend/MuzayedeBackendApplicationTests.java
+++ src/test/java/com/cengo/muzayedebackend/MuzayedeBackendApplicationTests.java
@@ -2,10 +2,26 @@ package com.cengo.muzayedebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
+@SpringBootTest(webEnvironment = DEFINED_PORT)
+@Testcontainers
 class MuzayedeBackendApplicationTests {
+	@Container
+	public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 	@Test
 	void contextLoads() {
 	}
