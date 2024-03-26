734e99dd953216684895fe855585a8d831012c8f
yagizpancak <yagizcan923@gmail.com>
2024-03-26 13:18:03 +0300

    feat: add user service & controller

---
+++ src/main/java/com/cengo/muzayedebackend/controller/contract/impl/UserControllerContractImpl.java
@@ -0,0 +1,44 @@
+package com.cengo.muzayedebackend.controller.contract.impl;
+
+import com.cengo.muzayedebackend.controller.contract.UserControllerContract;
+import com.cengo.muzayedebackend.dto.UserDTO;
+import com.cengo.muzayedebackend.mapper.UserMapper;
+import com.cengo.muzayedebackend.model.User;
+import com.cengo.muzayedebackend.request.UserSaveRequest;
+import com.cengo.muzayedebackend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UserControllerContractImpl implements UserControllerContract {
+	private final UserService userService;
+	@Override
+	public UserDTO saveUser(UserSaveRequest userSaveRequest) {
+		User user = UserMapper.INSTANCE.convertToUser(userSaveRequest);
+		user = userService.save(user);
+
+		return UserMapper.INSTANCE.convertToUserDTO(user);
+	}
+
+	@Override
+	public List<UserDTO> getAllUsers() {
+		List<User> userList = userService.findAll();
+
+		return UserMapper.INSTANCE.convertToUserDTOs(userList);
+	}
+
+	@Override
+	public UserDTO getUserById(Long id) {
+		User user = userService.findByIdWithControl(id);
+
+		return UserMapper.INSTANCE.convertToUserDTO(user);
+	}
+
+	@Override
+	public void deleteUser(Long id) {
+		userService.delete(id);
+	}
+}
---
+++ src/main/java/com/cengo/muzayedebackend/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.cengo.muzayedebackend.mapper;
+
+import com.cengo.muzayedebackend.dto.UserDTO;
+import com.cengo.muzayedebackend.model.User;
+import com.cengo.muzayedebackend.request.UserSaveRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+	User convertToUser(UserSaveRequest request);
+	UserDTO convertToUserDTO(User user);
+	List<UserDTO> convertToUserDTOs(List<User> users);
+}
